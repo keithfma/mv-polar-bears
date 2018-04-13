499b394713ac08a739f5049702db740ddcc3f200
Keith Ma <keith.ma@bostonfusion.com>
2018-04-13 17:26:45 -0400

    Started work on function to read data from Google Sheets

---
+++ dev.py
@@ -0,0 +1,12 @@
+"""
+Scratch space for developing key functions before it is clear where they will
+live in the final package
+"""
+
+def read_sheet():
+    """
+    Read attendence data from Google Sheets
+
+    Return: pandas dataframe containing current data
+    """
+    pass
