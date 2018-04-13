5144a4972a04f1bd7555f42b4d544727f48c9dfa
Keith Ma <keith.ma@bostonfusion.com>
2018-04-13 18:45:31 -0400

    First plot working

--- dev.py
+++ dev.py
@@ -5,8 +5,9 @@ live in the final package
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas
+import pandas as pd
 from numpy import nan
+from matplotlib import pyplot as plt
 
 
 # constants
@@ -29,13 +30,13 @@ def sheets_read_data():
     """
     Read attendence data from Google Sheets
 
-    Return: pandas dataframe containing current data
+    Return: pd dataframe containing current data
     """
     client = sheets_get_client(KEY_FILE)
     doc = client.open(DOC_TITLE)
     sheet = doc.worksheet(SHEET_TITLE)
     content = sheet.get_all_records(default_blank=nan)
-    return pandas.DataFrame(content)
+    return pd.DataFrame(content)
 
 
 def add_dates(data):
@@ -44,11 +45,36 @@ def add_dates(data):
 
     Return: Nothing, adds "DATE" column to dataframe
     """
-    data['DATE'] = pandas.to_datetime(data[['YEAR', 'MONTH', 'DAY']]).dt.date
+    data['DATE'] = pd.to_datetime(data[['YEAR', 'MONTH', 'DAY']]).dt.date
+
+
+def first_plot(data):
+    """Display attendence vs time plot"""
+    # constants
+    clr_group = 'b'
+    clr_newbies = 'g'
+
+    # plot years independently
+    label_group = 'Group'
+    label_newbies = 'Newbies'
+    years = data['YEAR'].unique()
+    for year in years:
+        year_data = data[data['YEAR'] == year]
+        plt.plot(year_data['DATE'], year_data['GROUP'], color=clr_group, label=label_group)
+        plt.plot(year_data['DATE'], year_data['NEWBIES'], color=clr_newbies, label=label_newbies)
+        label_group = '_nolegend_'
+        label_newbies = '_nolegend_'
+
+    plt.ylim(ymin=0)
+    plt.grid(linestyle=':')
+    plt.legend()
+    plt.title("Martha's Vineyard Polar Bears Attendence {} - {}".format(min(years), max(years)))
+    plt.show()
 
 
 # DEBUG: try out a few things
 if __name__ == '__main__':
     data = sheets_read_data()
     add_dates(data)
+    first_plot(data)
 
