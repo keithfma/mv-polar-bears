a680eb1cd869beedb7a3df867542d82094c91a29
Keith Ma <keithfma@gmail.com>
2018-08-08 22:24:28 -0400

    refreshed update_site script

--- update_site.sh
+++ update_site.sh
@@ -8,7 +8,7 @@ cd /home/keith/prj/mv-polar-bears
 branch_name=`git rev-parse --abbrev-ref HEAD`
 git checkout master
 python mvpb_data.py /home/keith/.mv-polar-bears/google_secret.json /home/keith/.mv-polar-bears/darksky_secret.json --log_level info
-python mvpb_site.py /home/keith/.mv-polar-bears/google_secret.json /home/keith/.mv-polar-bears/darksky_secret.json --log_level info
+python mvpb_site.py /home/keith/.mv-polar-bears/google_secret.json --log_level info
 git add docs/index.html docs/style.css
 git commit -m "Automatic update"
 git push
